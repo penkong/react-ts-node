0792c92c94adb84d15b7d342e59a0e8c1bd35f2f
mazdak <nazemi.works@gmail.com>
2021-01-25 00:04:06 +0400

    more on

---
+++ 233 013-bredux/src/components/code-cell.tsx
@@ -0,0 +1,38 @@
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import Resizable from './resizable';
+import { Cell } from '../state';
+import { useActions } from '../hooks/use-actions';
+import { useTypedSelector } from '../hooks/use-typed-selector';
+
+interface CodeCellProps {
+  cell: Cell;
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
+  const { updateCell } = useActions();
+  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  console.log(bundle);
+
+  return (
+    <Resizable direction="vertical">
+      <div
+        style={{
+          height: 'calc(100% - 10px)',
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
+          />
+        </Resizable>
+        <Preview code={bundle.code} err={bundle.err} />
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
